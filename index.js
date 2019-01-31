c7f1fac8f22beed810e74f364a3964b26d354df2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 15:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+function initForm() {
 
+}
 
+function handlebarsSetup() {
+
+}
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("partial-template").innerHTML)
+  handlebarsSetup()
+  initForm()
   Handlebars
 }
 document.addEventListener("DOMContentLoaded", function(event) {
