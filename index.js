9f65545aaf63366579081281c681899d228ae825
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 15:22:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function editRecipe() {
 
 Handlebars.registerPartial("partial-template", document.getElementById("recipe-template").innerHTML)
 
+function renderMain() {
+    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var html = template({name: "Gordon Ramsay"})
+}
+
+
+
 function createRecipe() {
 
     var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
