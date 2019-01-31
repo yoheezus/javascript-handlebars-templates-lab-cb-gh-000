d9aaa9ecdd7cfc7527b14323f31d6738bce6687a
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 15:30:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("part
 
 function renderMain() {
     var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var recipe = createRecipe();
     var html = template(recipe)
 
     document.getElementsByTagName("main")[0].innerHTML += html;
@@ -25,7 +26,11 @@ function createRecipe() {
     var recipeDescription = document.getElementById("recipe-description").value;
 
     var recipe = {
-        description: recipeDescription
+        description: recipeDescription,
+        ingredients: [
+            {quantity: "1 cup", name: "chicken"},
+            {quantity: "3 nanoliters", name: "stock"}
+        ]
     }
 
     return recipe
