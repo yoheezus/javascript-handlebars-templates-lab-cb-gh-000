be684e089658772a00aaecc0eae0a91fb3628129
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 16:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function initForm() {
     var formTemplate = document.getElementById("recipe-form-template").innerHTML
     var template = Handlebars.compile(formTemplate);
-    document.getElementsByTagName("main")[0].innerHTML =+ template({"submitAction": "createRecipe()"})
+    document.getElementsByTagName("main")[0].innerHTML += template({"submitAction": "createRecipe()"})
 }
 
 function createRecipe() {
