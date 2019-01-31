1af4ac0898143ade1b4a7ec00de0e49097878fa5
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 16:23:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function initForm() {
     var formTemplate = document.getElementById("recipe-form-template").innerHTML
     var template = Handlebars.compile(formTemplate);
-    document.getElementsByTagName("main")[0].innerHTML += template({"submitAction": "createRecipe()"})
+    document.getElementsByTagName("main")[0].innerHTML = template({"submitAction": "createRecipe()"})
 }
 
 function createRecipe() {
     var recipe = getRecipeVals()
     var recipeTemplate = document.getElementById("recipe-template").innerHTML
     var template = Handlebars.compile(recipeTemplate)
-    document.getElementById("main").innerHTML += template(recipe)
+    document.getElementById("main").innerHTML = template(recipe)
 }
 
 function updateRecipe() {
     var recipe = getRecipeVals()
     var recipeTemplate = document.getElementById("recipe-template").innerHTML
     var template = Handlebars.compile(recipeTemplate)
-    document.getElementById("main").innerHTML += template(recipe)
+    document.getElementById("main").innerHTML = template(recipe)
 }
 
 function displayEditForm() {
