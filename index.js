5841909e01aad07b1eecdf26b560ef2711da4b4e
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 15:16:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ Handlebars.registerHelper("displayIngredient",
 
 function createRecipe() {
 
-    var recipe ={
-        recipeName: recipeName,
-    }
     var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
     var recipeName = document.getElementById("recipe-name").value;
     var recipeDescription = document.getElementById("recipe-description").value;
+
+    var recipe ={
+        description: recipeDescription,
+        ingredients: [
+            {quantity: "1 cup", name: "chicken"},
+            {quantity: "3 nanoliters", name: "stock"}
+        ],
+
+    }
 
     var result = template(recipe);
     document.getElementsByTagName("main")[0].innerHTML += result;
